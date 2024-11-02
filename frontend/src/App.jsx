52f48fb85cb6a7edb44db53e3857e1e24edcb716
ramchandragawade <raajgawade39@gmail.com>
2024-11-03 02:52:36 +0530

    Main router done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import { Button } from './components/ui/button'
+import AuthLayout from './components/auth/layout'
+import AuthLogin from './pages/auth/login'
+import AuthRegister from './pages/auth/register'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <Button>Click!!!!</Button>
-    </>
+    <div className="flex flex-col overflow-hidden bg-white">
+      <h1>Header Cmp</h1>
+      <Routes>
+        <Route path='/auth' element={<AuthLayout/>}>
+          <Route path='login' element={<AuthLogin/>}/>
+          <Route path='register' element={<AuthRegister/>}/>
+        </Route>
+      </Routes>      
+    </div>
   )
 }
 
---
+++ frontend/src/pages/auth/register.jsx
@@ -0,0 +1,7 @@
+const AuthRegister = () => {
+  return (
+    <div>AuthRegister</div>
+  )
+}
+
+export default AuthRegister
