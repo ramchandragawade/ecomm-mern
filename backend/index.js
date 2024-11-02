e79d28e6a4a736fd11e06830671b7d8ec6c3db4f
ramchandragawade <raajgawade39@gmail.com>
2024-11-03 03:12:10 +0530

    .env setup

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
+require('dotenv').config();
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const mongoUrl = `mongodb+srv://raaj381995:raaj381995@cluster0.ukf0r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const mongoUrl = process.env.MONGOURL;
 
 const corsOrigin = `http://localhost:5173/`;
 
